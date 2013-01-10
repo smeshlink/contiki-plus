27bdac19ce6de4feb4ee92cd7f3fd6e5d1892b4b
longshine <longxianghe@gmail.com>
2013-01-10 21:26:13 +0800

    add sample 02 - mx web
    an simple ipv6-based web server.

---
+++ examples/02-mx-web/mx-web.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2011-2012 SmeshLink Technology Corporation.
+ * All rights reserved.
+ *
+ * $Id: mx-web.c $
+ */
+
+/**
+ * \file
+ *         An simple ipv6-based web server.
+ * \author
+ *         SmeshLink
+ */
+
+#include "webserver-nogui.h"
+
+/*---------------------------------------------------------------------------*/
+AUTOSTART_PROCESSES(&webserver_nogui_process);
+/*---------------------------------------------------------------------------*/
