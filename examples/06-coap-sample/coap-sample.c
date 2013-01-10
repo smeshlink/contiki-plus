3160999f7e42152f740d4ad5648da7afc71940c8
longshine <longxianghe@gmail.com>
2013-01-10 21:42:22 +0800

    add example resources in coap sample

--- examples/06-coap-sample/coap-sample.c
+++ examples/06-coap-sample/coap-sample.c
@@ -40,6 +40,65 @@
 /*---------------------------------------------------------------------------*/
 //char temp[100];
 
+PERIODIC_RESOURCE(temperature, METHOD_GET, "temperature", "title=\"Periodic temperature\";rt=\"Observable\"", 2*CLOCK_SECOND);
+void
+temperature_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	char tbuf[32];
+	int16_t temp = sensor_temp_get(TEMP_UNIT_CELCIUS);
+	sprintf(tbuf, "%d", temp);
+	REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+	REST.set_response_payload(response, (uint8_t *)tbuf, strlen(tbuf));
+}
+void
+temperature_periodic_handler(resource_t *r)
+{
+	static uint32_t obs_counter = 0;
+	char content[32];
+
+	obs_counter++;
+
+	int16_t temp = sensor_temp_get(TEMP_UNIT_CELCIUS);
+
+	/* Build notification. */
+	coap_packet_t notification[1]; /* This way the packet can be treated as pointer as usual. */
+	coap_init_message(notification, COAP_TYPE_NON, CONTENT_2_05, 0 );
+	coap_set_payload(notification, content, snprintf(content, sizeof(content), "%u", temp));
+
+	/* Notify the registered observers with the given message type, observe option, and payload. */
+	REST.notify_subscribers(r, obs_counter, notification);
+}
+
+/*---------------------------------------------------------------------------*/
+PERIODIC_RESOURCE(light, METHOD_GET, "light", "title=\"Periodic light\";rt=\"Observable\"", 1*CLOCK_SECOND);
+void
+light_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	char tbuf[32];
+	int16_t light = sensor_light_get();
+	sprintf(tbuf, "%d", light);
+	REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
+	REST.set_response_payload(response, (uint8_t *)tbuf, strlen(tbuf));
+}
+void
+light_periodic_handler(resource_t *r)
+{
+	static uint32_t obs_counter = 0;
+	char content[32];
+
+	obs_counter++;
+
+	int16_t light = sensor_light_get();
+
+	/* Build notification. */
+	coap_packet_t notification[1]; /* This way the packet can be treated as pointer as usual. */
+	coap_init_message(notification, COAP_TYPE_NON, CONTENT_2_05, 0 );
+	coap_set_payload(notification, content, snprintf(content, sizeof(content), "%u", light));
+
+	/* Notify the registered observers with the given message type, observe option, and payload. */
+	REST.notify_subscribers(r, obs_counter, notification);
+}
+
 /*---------------------------------------------------------------------------*/
 PERIODIC_RESOURCE(voltage, METHOD_GET, "voltage", "title=\"Periodic voltage\";rt=\"Observable\"", 1*CLOCK_SECOND);
 void
@@ -69,8 +128,143 @@ voltage_periodic_handler(resource_t *r)
 	/* Notify the registered observers with the given message type, observe option, and payload. */
 	REST.notify_subscribers(r, obs_counter, notification);
 }
+
 /*---------------------------------------------------------------------------*/
-#if UIP_CONF_IPV6_RPL
+RESOURCE(take, METHOD_GET | METHOD_POST, "take", "take");
+/*
+ * Take a picture.
+ */
+void
+take_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	const char *temp;
+	char tbuf[32] = "";
+	camera_size_t camera_size = CAMERA_SIZE_1;
+	uint16_t count;
+	uint32_t size;
+
+	/* Determine the size of picture. 1 for the smallest and 3 for the largest. */
+	if (!REST.get_query_variable(request, "size", &temp)) {
+		REST.get_post_variable(request, "size", &temp);
+	}
+
+	if (temp) {
+		camera_size = camera_parse_size(temp[0]);
+	}
+
+	camera_take_picture(camera_size, DEFAULT_CAMERA_PACKET_SIZE, &size, &count);
+	sprintf(tbuf, "{'t':'OK','s':%lu,'n':%u}", size, count);
+
+	REST.set_header_content_type(response, TEXT_PLAIN);
+	REST.set_response_payload(response, tbuf, strlen(tbuf));
+}
+
+RESOURCE(size, METHOD_GET, "size", "size");
+/*
+ * Get the size of the taken picture.
+ */
+void
+size_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	char tbuf[32] = "";
+
+	uint32_t size = camera_get_picture_size();
+	sprintf(tbuf, "%lu", size);
+
+	REST.set_header_content_type(response, TEXT_PLAIN);
+	REST.set_response_payload(response, tbuf, strlen(tbuf));
+}
+
+RESOURCE(count, METHOD_GET, "count", "count");
+/*
+ * Get the total count of the picture's packets.
+ */
+void
+count_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	char tbuf[32] = "";
+
+	uint16_t count = camera_get_packet_count();
+	sprintf(tbuf, "%u", count);
+
+	REST.set_header_content_type(response, TEXT_PLAIN);
+	REST.set_response_payload(response, tbuf, strlen(tbuf));
+}
+
+RESOURCE(packet, METHOD_GET | METHOD_PUT | METHOD_POST, "packet", "packet");
+/*
+ * Get one packet of the picture.
+ */
+void
+packet_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	const char *temp;
+	char tbuf[128];
+	int no = 0, len = 0, tries = 0;
+
+	/* Get the index of the specified packet. */
+	len = REST.get_query_variable(request, "no", &temp);
+	if (!len) {
+		len = REST.get_post_variable(request, "no", &temp);
+	}
+	memcpy(tbuf, temp, len);
+	tbuf[len] = '\0';
+	no = atoi(tbuf);
+
+	/* Get count of tries. Unused right now. */
+	len = REST.get_query_variable(request, "try", &temp);
+	if (!len) {
+		len = REST.get_post_variable(request, "try", &temp);
+	}
+	if (len) {
+		memcpy(tbuf, temp, len);
+		tbuf[len] = '\0';
+		tries = atoi(tbuf);
+	}
+	if (tries <= 0) {
+		tries = 10;
+	}
+	len = camera_try_get_packet(no, tbuf, tries);
+	REST.set_response_payload(response, tbuf, len);
+	REST.set_header_content_type(response, TEXT_PLAIN);
+}
+
+RESOURCE(camera, METHOD_GET | METHOD_POST, "camera", "camera");
+void
+camera_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	const char *temp;
+	uint8_t tbuf[128];
+	camera_size_t camera_size = CAMERA_SIZE_1;
+	u16_t count;
+	u32_t size;
+	int index = 0, len = 0, tries = 10;
+
+	if (0 == *offset) {
+		/* Determine the size of picture. 1 for the smallest and 3 for the largest. */
+		if (!REST.get_query_variable(request, "size", &temp)) {
+			REST.get_post_variable(request, "size", &temp);
+		}
+		if (temp) {
+			camera_size = camera_parse_size(temp[0]);
+		}
+		leds_toggle(LEDS_GREEN);
+		camera_take_picture(camera_size, preferred_size, &size, &count);
+	}
+
+	index = *offset / preferred_size + 1;
+	count = camera_get_packet_count();
+	len = camera_try_get_packet(index, tbuf, tries);
+	*offset += len;
+	if (index >= count) {
+		*offset = -1;
+	}
+	REST.set_header_content_type(response, REST.type.IMAGE_JPEG);
+	REST.set_response_payload(response, tbuf, len);
+}
+
+/*---------------------------------------------------------------------------*/
+#if UIP_CONF_IPV6_RPL && 0
 #include <avr/pgmspace.h>
 #include "rpl.h"
 
@@ -149,9 +343,16 @@ PROCESS_THREAD(coap_sample, ev, data)
 	rest_init_engine();
 
 	/* Activate the application-specific resources. */
+	rest_activate_resource(&resource_camera);
+	rest_activate_resource(&resource_take);
+	rest_activate_resource(&resource_size);
+	rest_activate_resource(&resource_count);
+	rest_activate_resource(&resource_packet);
 #if UIP_CONF_IPV6_RPL && 0
 	rest_activate_resource(&resource_route);
 #endif
+	rest_activate_periodic_resource(&periodic_resource_temperature);
+	rest_activate_periodic_resource(&periodic_resource_light);
 	rest_activate_periodic_resource(&periodic_resource_voltage);
 
 	PROCESS_END();
