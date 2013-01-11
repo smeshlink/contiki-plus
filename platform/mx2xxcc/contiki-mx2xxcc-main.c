4e5b25339081a1242111f50fa985e8f2db17f5c6
longshine <longxianghe@gmail.com>
2013-01-11 16:54:00 +0800

    merge contents of contiki-arduino.c into main.c

--- platform/mx2xxcc/contiki-mx2xxcc-main.c
+++ platform/mx2xxcc/contiki-mx2xxcc-main.c
@@ -99,6 +99,20 @@
 
 #if ARDUINO
 #include "contiki-arduino.h"
+
+unsigned char arduino_node_id[8] = { 0x02, 0x11, 0x22, 0xff, 0xfe, 0x33, 0x44, 0x33 };
+unsigned char arduino_channel = MXCHANNEL;
+unsigned char arduino_power = RF230_MAX_TX_POWER;
+
+PROCESS(contiki_arduino, "contiki arduino");
+AUTOSTART_PROCESSES(&contiki_arduino);
+
+PROCESS_THREAD(contiki_arduino, ev, data)
+{
+  PROCESS_BEGIN();
+  setup();
+  PROCESS_END();
+}
 #endif
 
 #ifdef CAMERA_INTERFACE
