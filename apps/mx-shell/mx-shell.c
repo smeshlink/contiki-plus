30bf3616181df3ff614fa2c747708dcef0d7c92b
longshine <longxianghe@gmail.com>
2013-04-23 17:31:21 +0800

    add app mx-shell
    To enable the mx-shell, add mx-shell to APPS in makefile, call
    mx_shell_init(RS232_PORT), and it's ready.

---
+++ apps/mx-shell/mx-shell.c
@@ -0,0 +1,172 @@
+/*
+ * Copyright (c) 2011-2013, SmeshLink Technology Ltd.
+ * All rights reserved.
+ *
+ * $Id: mx-shell.c $
+ */
+
+/**
+ * \file
+ *         A shell back-end for the serial port.
+ * \author
+ *         SmeshLink
+ */
+
+#include "mx-shell.h"
+#include "serial-shell.h"
+
+#include "radio.h"
+#if RF230BB || RF212BB
+#include "rf230bb.h"
+#endif
+
+#include "params.h"
+#include "dev/rs232.h"
+#include "dev/serial-line.h"
+
+static char buf[64];
+/*---------------------------------------------------------------------------*/
+PROCESS(shell_txpower_process, "txpower");
+SHELL_COMMAND(txpower_command,
+          "txpower",
+          "txpower <power>: change RF transmission power (0 - 15)",
+          &shell_txpower_process);
+PROCESS(shell_rfchannel_process, "rfchannel");
+SHELL_COMMAND(rfchannel_command,
+          "rfchannel",
+          "rfchannel <channel>: change RF channel (11 - 26)",
+          &shell_rfchannel_process);
+PROCESS(shell_debug_process, "debug");
+SHELL_COMMAND(debug_command,
+          "debug",
+          "debug <state>: toggle debug output (on / off)",
+          &shell_debug_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(shell_txpower_process, ev, data)
+{
+  uint16_t txpower;
+  const char *newptr;
+
+  PROCESS_BEGIN();
+
+  txpower = shell_strtolong(data, &newptr);
+
+  /* If no transmission power was given on the command line, we print
+     out the current txpower. */
+  if(newptr == data) {
+    txpower = rf230_get_txpower();
+    sprintf(buf, "%d", txpower);
+    shell_output_str(&txpower_command, "Current TX power (0=+3dBm, 15=-17.2dBm) is ", buf);
+  } else if ((txpower < 0) || (txpower > 15)) {
+    shell_output_str(&txpower_command, "Invalid TX power", "");
+  } else {
+    rf230_set_txpower(txpower);
+    sprintf(buf, "Transmit power changed to %d", txpower);
+#if CONTIKI_CONF_SETTINGS_MANAGER
+    if(settings_set_uint8(SETTINGS_KEY_TXPOWER, txpower) == SETTINGS_STATUS_OK) {
+      shell_output_str(&txpower_command, buf, " and stored in EEPROM.");
+    } else {
+      shell_output_str(&txpower_command, buf, ", but unable to store in EEPROM!");
+    }
+#else
+    shell_output_str(&txpower_command, buf, "");
+#endif
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(shell_rfchannel_process, ev, data)
+{
+  uint16_t channel;
+  const char *newptr;
+
+  PROCESS_BEGIN();
+
+  channel = shell_strtolong(data, &newptr);
+
+  /* If no channel was given on the command line, we print out the
+     current channel. */
+  if(newptr == data) {
+    channel = rf230_get_channel();
+    sprintf(buf, "%d", channel);
+    shell_output_str(&rfchannel_command, "Current channel is ", buf);
+  } else if ((channel < 11) || (channel > 26)) {
+    shell_output_str(&rfchannel_command, "Invalid channel", "");
+  } else {
+    rf230_set_channel(channel);
+    sprintf(buf, "Channel changed to %d", channel);
+#if CONTIKI_CONF_SETTINGS_MANAGER
+    if(settings_set_uint8(SETTINGS_KEY_CHANNEL, channel) == SETTINGS_STATUS_OK) {
+      shell_output_str(&rfchannel_command, buf, " and stored in EEPROM.");
+    } else {
+      shell_output_str(&rfchannel_command, buf, ", but unable to store in EEPROM!");
+    }
+#else
+    shell_output_str(&rfchannel_command, buf, "");
+#endif
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(shell_debug_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  if (strncasecmp(data, "on", 2) == 0) {
+    shell_mode.debugOn = 1;
+  } else if (strncasecmp(data, "off", 3) == 0) {
+    shell_mode.debugOn = 0;
+  }
+
+  shell_output_str(&debug_command, "Debug strings are ",
+      shell_mode.debugOn ? "on" : "off");
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+static int
+serial_input_byte(unsigned char c)
+{
+  int ret = 0;
+
+  stdout = shell_mode.serial_stdout;
+  printf("%c", c);
+
+  if (c == 0x0d) {
+    c = 0x0a;
+    printf("\r\n");
+  } else if (c == 0x0a) {
+    c = 0x0d;
+  }
+  ret = serial_line_input_byte(c);
+
+  if (!shell_mode.debugOn) {
+    stdout = NULL;
+  }
+
+  return ret;
+}
+/*---------------------------------------------------------------------------*/
+void
+mx_shell_init(uint8_t port)
+{
+  rs232_init(port, USART_BAUD_38400, USART_PARITY_NONE | USART_STOP_BITS_1 | USART_DATA_BITS_8);
+  rs232_set_input(port, serial_input_byte);
+
+  shell_mode.serial_stdout = stdout;
+  shell_mode.debugOn = 0;
+
+  serial_line_init();
+  serial_shell_init();
+
+  shell_blink_init();
+  shell_ps_init();
+  shell_reboot_init();
+  shell_power_init();
+  shell_time_init();
+  shell_register_command(&txpower_command);
+  shell_register_command(&rfchannel_command);
+  shell_register_command(&debug_command);
+}
---
+++ apps/mx-shell/mx-shell.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2011-2013, SmeshLink Technology Ltd.
+ * All rights reserved.
+ *
+ * $Id: mx-shell.h $
+ */
+
+/**
+ * \file
+ *         A shell back-end for the serial port.
+ * \author
+ *         SmeshLink
+ */
+
+#ifndef __MX_SHELL_H__
+#define __MX_SHELL_H__
+
+#include "shell.h"
+
+void mx_shell_init(uint8_t port);
+
+#endif /* __MX_SHELL_H__ */
